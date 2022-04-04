988b50ee0481b2fac5f3f216e1d6731b4079ff2c
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-04-05 01:13:55 +0200

    ajout polify

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const { UseBuiltInsOption } = require("@babel/preset-env/lib/options");
+
 module.exports = {
     presets: [
-        ["@babel/preset-env"]
+        [
+            "@babel/preset-env", {
+                UseBuiltIns: "usage",
+                corejs: 3,
+                debug: true,
+                targets: ">30%, not dead"
+            }
+        ]
     ]
 };
