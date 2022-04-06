7143e1f3c2adde1bb246868bdc82fbf169d7d032
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-04-06 17:40:42 +0200

    introduction à formData

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-const { async } = require("regenerator-runtime");
+// const { async } = require("regenerator-runtime");
+import { even } from "../dist/main.bundle";
 
-const user = {
-    name: "tofik",
-    email: "kipo@brief.fr"
-};
+// const user = {
+//     name: "tofik",
+//     email: "kipo@brief.fr"
+// };
 
-const promesse = fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
+// const promesse = fetch("https://jsonplaceholder.typicode.com/users", {
+//     method: "POST",
+//     body: JSON.stringify(user),
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// });
 
-promesse.then(async response => {
-    try {
-        console.log(response);
-        const body = await response.json();
-        console.log(body);
-    } catch (e) {
-        console.log(e);
-    }
-});
+// promesse.then(async response => {
+//     try {
+//         console.log(response);
+//         const body = await response.json();
+//         console.log(body);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// });
 
 // promesse.then(async response => {
 //     try {
@@ -33,3 +34,24 @@ promesse.then(async response => {
 // }).catch(error => {
 //     console.log(error);
 // });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    formData.append("name", "value");
+    formData.set("email", "email@testing.com");
+    console.log(formData.get("email"));
+
+    for (let pair of formData) {
+        console.log(pair);
+    }
+
+    fetch("/test", {
+        method: "POST",
+        body: formData
+    });
+});
