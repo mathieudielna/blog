311447a081d52405be3bfb8b31fd2d54ac0bf0e6
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-04-08 15:28:43 +0200

    init navigation

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry: {
+        main: path.join(__dirname, "src/index.js"),
+        form: path.join(__dirname, "src/form/form.js")
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].bundle.js"
@@ -14,12 +17,22 @@ module.exports = {
             use: ["babel-loader"]
         }, {
             test: /\.scss$/i,
-            use: ["style-loader", "css-loader"]
+            use: ["style-loader", "css-loader", "sass-loader"]
         }]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/index.html")
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.join(__dirname, "./src/index.html"),
+            chunks: ["main"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: "form.html",
+            template: path.join(__dirname, "./src/form/form.html"),
+            chunks: ["form"]
+        })
+    ],
+    stats: "minimal",
     devtool: "source-map",
     mode: "development",
     devServer: {
