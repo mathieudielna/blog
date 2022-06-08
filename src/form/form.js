9f3caef367bb966c075ff20c25a63c02894ac327
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-06-08 08:18:26 +0200

    style + formulaires ok

--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,58 @@
 import "./form.scss";
 import "../assets/styles/styles.scss";
 
-console.log("form");
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", async event => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const entries = formData.entries();
+    const obj = Object.fromEntries(entries);
+    if (formIsValid(obj)) {
+        try {
+            const json = JSON.stringify(obj);
+            const response = await fetch("https://restapi.fr/api/blogmd", {
+                method: "POST",
+                body: json,
+                headers: {
+                    "Content-type": "application/json"
+                }
+            });
+            const body = await response.json();
+            console.log(body);
+        } catch (e) {
+            console.error("erreur : ", e);
+        }
+
+        // console.log(entries);
+        // const obj = Array.from(entries).reduce((acc, value) => {
+        //     acc[value[0]] = value[1];
+        //     return acc;
+        // }, {});
+
+        // for (let entry of entries) {
+        //     console.log(entry);
+        // }
+    }
+});
+
+const formIsValid = obj => {
+    if (!obj.author || !obj.category || !obj.content) {
+        errors.push("Tout les champs doivent être complèter!");
+    } else {
+        errors = [];
+    }
+    if (errors.length) {
+        let errorHTML = "";
+        errors.forEach(e => {
+            errorHTML += `<li>${e}</li>`;
+        });
+        errorElement.innerHTML = errorHTML;
+        return false;
+    } else {
+        errorElement.innerHTML = "";
+        return true;
+    }
+};
